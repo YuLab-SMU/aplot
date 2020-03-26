b621b01ed41b7ed29024efccda2af1897a04e20e
Guangchuang Yu <guangchuangyu@gmail.com>
2020-03-26 21:02:25 +0800

    aplot initial version

---
+++ R/aplot.R
@@ -0,0 +1,44 @@
+
+as.aplot <- function(plot) {
+    if (inherits(plot, "aplot"))
+        return(plot)
+    
+    if (!inherits(plot, 'gg')) {
+        stop("input should be a 'gg' object.")  
+    }
+    structure(list(plotlist = list(plot),
+                   width = 1,
+                   height = 1,
+                   layout = matrix(1),
+                   n = 1,
+                   main_col = 1,
+                   main_row = 1),
+              class = "aplot")
+    
+}
+
+
+##' @method print aplot
+##' @export
+print.aplot <- function(x, ...) {
+    idx <- as.vector(x$layout)
+    idx[is.na(idx)] <- x$n + 1 
+    x$plotlist[[x$n+1]] <- plot_spacer()
+    plotlist <- x$plotlist[idx]
+    
+    pp <- plotlist[[1]]
+    np <- length(plotlist)
+    if (np == 1) {
+        return(pp)
+    }
+    
+    for (i in 2:length(plotlist)) {
+        pp <- pp + plotlist[[i]]
+    }
+    
+    pp + plot_layout(byrow=F, ncol=ncol(x$layout), 
+                     widths = x$width,
+                     heights= x$height,
+                     guides = 'collect')
+    
+}
---
+++ R/insertion.R
@@ -0,0 +1,117 @@
+##' insert an associated plot to left, right, top and bottom of a main plot
+##'
+##' The first input serve as a main plot, and other plots can be progressively inserted to
+##' different sides on left, right, top and bottom.
+##' @title plot-insertion
+##' @rdname plot-insertion
+##' @param .data an 'aplot' or 'gg' object
+##' @param plot a 'gg' plot to be inserted
+##' @param width relative width to the main plot
+##' @param height relative height to the main plot
+##' @return an 'aplot' object
+##' @export
+##' @author Guangchuang Yu
+insert_left <- function(.data, plot, width=1) {
+  .data <- as.aplot(.data)
+  .data$n <- .data$n + 1
+  .data$width <- c(width, .data$width)
+  new_col <- matrix(nrow=nrow(.data$layout), ncol=1)
+  new_col[.data$layout[, .data$main_col] == 1] = .data$n
+  .data$layout <- cbind(new_col, .data$layout)
+  .data$main_col <- .data$main_col + 1
+  
+  plot <- plot + ggtree::ylim2(.data$plotlist[[1]])
+  
+  if (inherits(plot, "ggtree")) { ## re-order based on the tree
+    yvar <- rvcheck::get_aes_var(main_plot$mapping, 'y')
+    for (i in 1:length(.data$plotlist)) {
+      .data$plotlist[[i]] <- .data$plotlist[[i]] + 
+            aes(y = factor(.data[[yvar]], 
+                        levels = rev(ggtree::get_taxa_name(plot)))) +
+            ylab(NULL)
+    }
+  }
+
+  .data$plotlist[[.data$n]] = plot 
+  .data
+}
+
+##' @rdname plot-insertion
+##' @export
+insert_right <- function(.data, plot, width=1) {
+  .data <- as.aplot(.data)
+  .data$n <- .data$n + 1
+  .data$width <- c(.data$width, width)
+  new_col <- matrix(nrow=nrow(.data$layout), ncol=1)
+  new_col[.data$layout[, .data$main_col] == 1] = .data$n
+  .data$layout <- cbind(.data$layout, new_col)
+  
+  plot <- plot + ggtree::ylim2(.data$plotlist[[1]])
+  
+  if (inherits(plot, "ggtree")) { ## re-order based on the tree
+    yvar <- rvcheck::get_aes_var(main_plot$mapping, 'y')
+    for (i in 1:length(.data$plotlist)) {
+      .data$plotlist[[i]] <- .data$plotlist[[i]] + 
+        aes(y = factor(.data[[yvar]], 
+                       levels = rev(ggtree::get_taxa_name(plot)))) +
+        ylab(NULL)
+    }
+  }
+  
+  .data$plotlist[[.data$n]] = plot 
+  .data
+}
+
+##' @rdname plot-insertion
+##' @export
+insert_top <- function(.data, plot, height=1) {
+  .data <- as.aplot(.data)
+  .data$n <- .data$n + 1
+  .data$height <- c(height, .data$height)
+  new_row <- matrix(nrow=1, ncol=ncol(.data$layout))
+  new_row[.data$layout[.data$main_row, ] == 1] = .data$n
+  .data$layout <- rbind(new_row, .data$layout)
+  .data$main_row <- .data$main_row + 1
+  
+  plot <- plot + ggtree::xlim2(.data$plotlist[[1]])
+  
+  if (inherits(plot, "ggtree")) { ## re-order based on the tree
+    xvar <- rvcheck::get_aes_var(main_plot$mapping, 'x')
+    for (i in 1:length(.data$plotlist)) {
+      .data$plotlist[[i]] <- .data$plotlist[[i]] + 
+        aes(x = factor(.data[[xvar]], 
+                       levels = rev(ggtree::get_taxa_name(plot)))) +
+        xlab(NULL)
+    }
+  }
+  
+  .data$plotlist[[.data$n]] = plot 
+  .data
+}
+
+
+##' @rdname plot-insertion
+##' @export
+insert_bottom <- function(.data, plot, height=1) {
+  .data <- as.aplot(.data)
+  .data$n <- .data$n + 1
+  .data$height <- c(.data$height, height)
+  new_row <- matrix(nrow=1, ncol=ncol(.data$layout))
+  new_row[.data$layout[.data$main_row, ] == 1] = .data$n
+  .data$layout <- rbind(.data$layout, new_row)
+  
+  plot <- plot + ggtree::xlim2(.data$plotlist[[1]])
+  
+  if (inherits(plot, "ggtree")) { ## re-order based on the tree
+    xvar <- rvcheck::get_aes_var(main_plot$mapping, 'x')
+    for (i in 1:length(.data$plotlist)) {
+      .data$plotlist[[i]] <- .data$plotlist[[i]] + 
+        aes(x = factor(.data[[xvar]], 
+                       levels = rev(ggtree::get_taxa_name(plot)))) +
+        xlab(NULL)
+    }
+  }
+  
+  .data$plotlist[[.data$n]] = plot 
+  .data
+}
